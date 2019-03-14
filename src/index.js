833acdbb4cf56e1ad9c1b3ad3420443d0a85d004
Mads Wulff Nielsen <mwn@Madss-MacBook-Pro.local>
2019-03-14 20:11:12 +0100

    Delete method implemented

--- src/index.js
+++ src/index.js
@@ -5,12 +5,10 @@ var input = document.createElement("input");
 const searchBtn = document.createElement("button");
 const radioDiv = document.createElement("div");
 const resultDiv = document.createElement("div")
-var checkEventListened = false;
 
 const mainUrl = "http://localhost:8084/api/";
 
-var radioGroup = ["Persons by Address", "Person by Phone", "Persons by Hobby", "Person by Zip", "Count by Hobby"];
-
+var radioGroup = ["Persons by Address", "Person by Phone", "Persons by Hobby", "Person by Zip", "Count by Hobby", "Delete Person"];
 
 radioDiv.innerHTML = generateRadios(radioGroup);
 input.setAttribute("id", "input");
@@ -26,43 +24,64 @@ root.appendChild(resultDiv);
 function gettingFunction(idString) {
     switch (idString) {
         case "PersonsbyAddress":
+            searchBtn.innerHTML = "Search";
             input.setAttribute("placeholder", "Search Address");
             searchBtn.addEventListener("click", getPersonsByAddress);
             searchBtn.removeEventListener("click", getPersonByPhone);
             searchBtn.removeEventListener("click", getPersonsByHobby);
             searchBtn.removeEventListener("click", getPersonByZip);
             searchBtn.removeEventListener("click", getCountByHobby);
+            searchBtn.removeEventListener("click", deletePersonByPhone);
             break;
         case "PersonbyPhone":
+            searchBtn.innerHTML = "Search";
             input.setAttribute("placeholder", "Search Phone");
             searchBtn.addEventListener("click", getPersonByPhone);
             searchBtn.removeEventListener("click", getPersonsByAddress);
             searchBtn.removeEventListener("click", getPersonsByHobby);
             searchBtn.removeEventListener("click", getPersonByZip);
             searchBtn.removeEventListener("click", getCountByHobby);
+            searchBtn.removeEventListener("click", deletePersonByPhone);
             break;
         case "PersonsbyHobby":
+            searchBtn.innerHTML = "Search";
             input.setAttribute("placeholder", "Search Hobby");
             searchBtn.addEventListener("click", getPersonsByHobby);
             searchBtn.removeEventListener("click", getPersonByPhone);
             searchBtn.removeEventListener("click", getPersonsByAddress);
             searchBtn.removeEventListener("click", getPersonByZip);
             searchBtn.removeEventListener("click", getCountByHobby);
+            searchBtn.removeEventListener("click", deletePersonByPhone);
             break;
         case "PersonbyZip":
+            searchBtn.innerHTML = "Search";
             input.setAttribute("placeholder", "Search Zip");
             searchBtn.addEventListener("click", getPersonByZip);
             searchBtn.removeEventListener("click", getPersonByPhone);
             searchBtn.removeEventListener("click", getPersonsByHobby);
             searchBtn.removeEventListener("click", getPersonsByAddress);
             searchBtn.removeEventListener("click", getCountByHobby);
+            searchBtn.removeEventListener("click", deletePersonByPhone);
             break;
         case "CountbyHobby":
+            searchBtn.innerHTML = "Search";
+            input.setAttribute("placeholder", "Enter Hobby");
             searchBtn.addEventListener("click", getCountByHobby);
             searchBtn.removeEventListener("click", getPersonByPhone);
             searchBtn.removeEventListener("click", getPersonsByHobby);
             searchBtn.removeEventListener("click", getPersonByZip);
             searchBtn.removeEventListener("click", getPersonsByAddress);
+            searchBtn.removeEventListener("click", deletePersonByPhone);
+            break;
+        case "DeletePerson":
+            searchBtn.innerHTML = "Delete";
+            input.setAttribute("placeholder", "Enter Phonenumber");
+            searchBtn.addEventListener("click", deletePersonByPhone);
+            searchBtn.removeEventListener("click", getPersonByPhone);
+            searchBtn.removeEventListener("click", getPersonsByHobby);
+            searchBtn.removeEventListener("click", getPersonByZip);
+            searchBtn.removeEventListener("click", getPersonsByAddress);
+            searchBtn.removeEventListener("click", getCountByHobby);
             break;
 
     }
@@ -238,6 +257,7 @@ function generateRadios(properties) {
 
 function deletePersonByPhone() {
     const phone = input.value;
+
     const totalUrl = mainUrl + "person/delete/";
     fetch(totalUrl + phone, {
         method: 'delete',
@@ -246,7 +266,11 @@ function deletePersonByPhone() {
             'Content-Type': 'application/json; charset=utf-8'
         },
         body: JSON.stringify(phone)
-    }).then(res => res.json());
+    }).then(res => {
+        if (!res.ok) {
+            return Promise.reject({ status: res.status, fullError: res.json() })
+        }
+    input.value = "";
 }
 /*
 function createPerson(){
